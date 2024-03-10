aa5b8e9b52b1d732c244c9a04b992022b740ca82
Riley-Cameron <astoriacamfam@gmail.com>
2024-03-10 16:48:33 -0700

    --

--- motor_control_package/motor_control_package/motor_drivers.py
+++ motor_control_package/motor_control_package/motor_drivers.py
@@ -12,8 +12,8 @@ class MotorDriver(Node):
     def __init__(self):
         self.motors = []
 
-        self.motors.append(PWM(topic='/motor/tread_left', pin=16, freq=50, min_dc=0, max_dc=100, init_range=0))
-        self.motors.append(PWM(topic='/motor/tread_right', pin=16, freq=50, min_dc=0, max_dc=100, init_range=0))
+        self.motors.append(PWM(topic='/motor/tread_left', pin=16, freq=1000, min_dc=0, max_dc=90, init_range=0))
+        self.motors.append(PWM(topic='/motor/tread_right', pin=16, freq=1000, min_dc=0, max_dc=90, init_range=0))
         self.get_logger().info("Initialized Drive Motors")
 
         self.motors.append(PWM(topic='/motor/arm_angle_joint', pin=16, freq=50, min_dc=0, max_dc=100, init_range=0))
