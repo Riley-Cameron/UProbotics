0e15729f42088b99e399645b230f20abe5073adf
Riley-Cameron <astoriacamfam@gmail.com>
2024-03-26 17:46:54 -0700

    BIG CHANGE in motor_drivers.py

--- motor_control_package/motor_control_package/motor_drivers.py
+++ motor_control_package/motor_control_package/motor_drivers.py
@@ -1,43 +1,37 @@
 #!/usr/bin/env python3
-
+from MotorListener import MotorListener
 from BLDC import BLDC
 from Actuator import Actuator
-
-from std_msgs.msg import Float64
 import rclpy
-from rclpy.node import Node
 import RPi.GPIO as GPIO
 
-class MotorDriver(Node):
-    
-    def __init__(self):
-        super().__init__("motor_drivers")
-        self.motors = []
-
-        self.motors.append(BLDC(topic='/motor/tread_left', node='left_tread', pin=12, dir_pin=16, en_pin=20, freq=50, min_dc=0, max_dc=90, init_range=0))
-        self.motors.append(BLDC(topic='/motor/tread_right', node='right_tread', pin=13, dir_pin=6, en_pin=5, freq=50, min_dc=0, max_dc=90, init_range=0))
-        self.get_logger().info("Initialized Drive Motors")
-
-        self.motors.append(Actuator(topic='/motor/arm_angle_joint', node='arm_joint', pinA=23, pinB=24))
-        self.get_logger().info("Initialized Arm Articulation")
-
-        self.motors.append(Actuator(topic='/motor/bucket_angle_joint', node='bucket_joint', pinA=7, pinB=8))
-        self.get_logger().info("Initialized Bucket Articulation")
-
-        for motor in self.motors:
-            motor.start()
-
+        
 def main(args=None):
     rclpy.init(args=args)
-    md = MotorDriver()
+    
+    motors = []
 
-    try:
-        rclpy.spin(md)
-    except KeyboardInterrupt:
-        pass
+    motors.append(BLDC(topic='/motor/tread_left', node='left_tread', pin=12, dir_pin=16, en_pin=20, freq=50, min_dc=0, max_dc=90, init_range=0))
+    motors.append(BLDC(topic='/motor/tread_right', node='right_tread', pin=13, dir_pin=6, en_pin=5, freq=50, min_dc=0, max_dc=90, init_range=0))
 
-    for motor in md.motors:
-        motor.destroy_node()
+    motors.append(Actuator(topic='/motor/arm_angle_joint', node='arm_joint', pinA=23, pinB=24))
+
+    motors.append(Actuator(topic='/motor/bucket_angle_joint', node='bucket_joint', pinA=7, pinB=8))
+
+    for motor in motors:
+        motor.start()
+
+    for motor in motors:
+        try:
+            rclpy.spin(motor)
+        except KeyboardInterrupt:
+            pass
+
+    for motor in motors:
+        motor.stop()
+
+    for motor in motors:
+        motor.join()
 
     GPIO.cleanup()
     rclpy.shutdown()
