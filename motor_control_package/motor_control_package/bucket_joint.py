44e5f09f9add17d348b79e6d9ee08ddedc8f3b42
Riley-Cameron <astoriacamfam@gmail.com>
2024-03-26 18:44:55 -0700

    added individual scripts to launch nodes

--- motor_control_package/motor_control_package/bucket_joint.py
+++ motor_control_package/motor_control_package/bucket_joint.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-from BLDC import BLDC
+from Actuator import Actuator
 import rclpy
 import RPi.GPIO as GPIO
 
 def main(args=None):
     rclpy.init(args=args) #initialize ros2 communication
 
-    node = BLDC(topic='/motor/tread_left', node='left_tread', pin=12, dir_pin=16, en_pin=20, freq=50, min_dc=0, max_dc=90, init_range=0) #instantiate a node (starts node)
+    node = Actuator(topic='/motor/bucket_angle_joint', node='bucket_joint', pinA=7, pinB=8) #instantiate a node (starts node)
     node.start()
 
     rclpy.spin(node) # .spin runs a node until it is manually killed
