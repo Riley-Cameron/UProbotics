a9a2a8f3120d8eb3dedd642912c919b0c21bbf36
Riley-Cameron <astoriacamfam@gmail.com>
2024-03-26 18:31:00 -0700

    left tread node

---
+++ motor_control_package/motor_control_package/left_tread.py
@@ -0,0 +1,14 @@
+from BLDC import BLDC
+import rclpy
+import RPi.GPIO as GPIO
+
+def main(args=None):
+    rclpy.init(args=args) #initialize ros2 communication
+    node = BLDC(topic='/motor/tread_left', node='left_tread', pin=12, dir_pin=16, en_pin=20, freq=50, min_dc=0, max_dc=90, init_range=0) #instantiate a node (starts node)
+    rclpy.spin(node) # .spin runs a node until it is manually killed
+
+    GPIO.cleanup()
+    rclpy.shutdown() # end ros2 comms
+
+if __name__ == '__main__':
+    main()
